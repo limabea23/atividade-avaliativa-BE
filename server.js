381ebfc3900cc2a286b098dba26854ff9a92c2ac
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-05-06 11:03:14 -0300

    feat: API Key with middleware

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const cors = require("cors");
 const contaRoutes = require("./src/routes/contaRoutes.js");
 const pagamentoRoutes = require("./src/routes/pagamentoRoutes.js");
 const path = require("path");
+const apiKeyMiddleware = require('./src/config/apiKey.js');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(apiKeyMiddleware);
 
 app.use("/api", contaRoutes); 
 app.use("/api", pagamentoRoutes); 
